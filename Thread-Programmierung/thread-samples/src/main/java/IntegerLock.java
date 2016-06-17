17ca84019357a4129249ef23ba47d412b2b52aff
Lucas Hauswald <lucas@the-soulmen.com>
2016-06-17 16:33:06 +0200

    Replaces spaces with tabs in latest code commit

--- Thread-Programmierung/thread-samples/src/main/java/IntegerLock.java
+++ Thread-Programmierung/thread-samples/src/main/java/IntegerLock.java
@@ -5,76 +5,76 @@ import java.util.concurrent.locks.Lock;
 
 public class IntegerLock extends AtomicInteger implements Lock {
 
-    public static void main(String[] args) {
-        IntegerLock lockingInt=new IntegerLock(0);
-        Thread threadOne = new Thread(
-                () -> {
-                    lockingInt.lock();
-                    lockingInt.getAndDecrement();
-                    System.out.println("one:"+lockingInt);
-                    lockingInt.unlock();
-                });
-        Thread threadTwo = new Thread(
-                () -> {
-                    lockingInt.getAndDecrement();
-                    System.out.println("two:"+lockingInt);
-                });
-        Thread threadThree = new Thread(
-                () -> {
-                    lockingInt.lock();
-                    lockingInt.getAndIncrement();
-                    lockingInt.getAndIncrement();
-                    lockingInt.getAndIncrement();
-                    lockingInt.unlock();
-                    System.out.println("three:"+lockingInt);
-                });
-        threadOne.run();
-        threadTwo.run();
-        threadThree.run();
-    }
+	public static void main(String[] args) {
+		IntegerLock lockingInt = new IntegerLock(0);
+		Thread threadOne = new Thread(
+				() -> {
+					lockingInt.lock();
+					lockingInt.getAndDecrement();
+					System.out.println("one:" + lockingInt);
+					lockingInt.unlock();
+				});
+		Thread threadTwo = new Thread(
+				() -> {
+					lockingInt.getAndDecrement();
+					System.out.println("two:" + lockingInt);
+				});
+		Thread threadThree = new Thread(
+				() -> {
+					lockingInt.lock();
+					lockingInt.getAndIncrement();
+					lockingInt.getAndIncrement();
+					lockingInt.getAndIncrement();
+					lockingInt.unlock();
+					System.out.println("three:" + lockingInt);
+				});
+		threadOne.run();
+		threadTwo.run();
+		threadThree.run();
+	}
 
-    public IntegerLock(int i) {
-        super(i);
-    }
+	public IntegerLock(int i) {
+		super(i);
+	}
 
-    public IntegerLock() {
-        super(0);
-    }
+	public IntegerLock() {
+		super(0);
+	}
 
-    public void lock() {
-        //b==0 -> wir sind die ersten
-        int b = this.getAndIncrement();
-        while (b > 0) {
-            //werden unter gegenseitigen Ausschluss aufgerufen
-            //-> funktioniert bei Aufruf eines anderen nicht bei einem selbst
-            System.out.println(b);
-            getAndDecrement();
-            b = this.getAndIncrement();
-        }
-    }
+	public void lock() {
+		//b==0 -> wir sind die ersten
+		int b = this.getAndIncrement();
+		while (b > 0) {
+			//werden unter gegenseitigen Ausschluss aufgerufen
+			//-> funktioniert bei Aufruf eines anderen nicht bei einem selbst
+			System.out.println(b);
+			getAndDecrement();
+			b = this.getAndIncrement();
+		}
+	}
 
-    @Override
-    public void lockInterruptibly() throws InterruptedException {
+	@Override
+	public void lockInterruptibly() throws InterruptedException {
 
-    }
+	}
 
-    @Override
-    public boolean tryLock() {
-        return false;
-    }
+	@Override
+	public boolean tryLock() {
+		return false;
+	}
 
-    @Override
-    public boolean tryLock(long l, TimeUnit timeUnit) throws InterruptedException {
-        return false;
-    }
+	@Override
+	public boolean tryLock(long l, TimeUnit timeUnit) throws InterruptedException {
+		return false;
+	}
 
-    @Override
-    public void unlock() {
-        this.getAndDecrement();
-    }
+	@Override
+	public void unlock() {
+		this.getAndDecrement();
+	}
 
-    @Override
-    public Condition newCondition() {
-        return null;
-    }
+	@Override
+	public Condition newCondition() {
+		return null;
+	}
 }
